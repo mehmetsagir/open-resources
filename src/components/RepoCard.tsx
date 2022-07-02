81627a00b8bb466d1047a88410837d9c55303a3b
mehmetsagir <memetsagirint@gmail.com>
2022-07-03 01:49:27 +0300

    add Repo Card

---
+++ src/components/RepoCard.tsx
@@ -0,0 +1,115 @@
+import styled from 'styled-components';
+
+import repoLanguageColor from '/public/assets/repo-language-color.json';
+import GithubForkIcon from '/public/svg/github-fork.svg';
+import GithubRepoIcon from '/public/svg/github-repo.svg';
+import GithubStarIcon from '/public/svg/github-star.svg';
+
+type Repo = {
+  repo: string;
+  link: string;
+  description: string;
+  language: string;
+  stars: number;
+  forks: number;
+};
+
+type Props = {
+  repo: Repo;
+};
+
+const RepoCard: React.FC<Props> = ({ repo }) => (
+  <Container>
+    <div className="title">
+      <GithubRepoIcon />
+      <a href={repo.link} target="_blank" rel="noreferrer">
+        {repo.repo}
+      </a>
+    </div>
+    <p className="description">{repo.description}</p>
+    <div className="info">
+      <div>
+        <span
+          className="language-color"
+          style={{
+            // @ts-ignore
+            backgroundColor: repoLanguageColor[repo.language],
+          }}
+        />
+        <span>{repo.language}</span>
+      </div>
+      {repo.stars > 0 && (
+        <a href={`${repo.link}/stargazers`} target="_blank" rel="noreferrer">
+          <GithubStarIcon />
+          <span>{repo.stars}</span>
+        </a>
+      )}
+      {repo.forks > 0 && (
+        <a
+          href={`${repo.link}/network/members`}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <GithubForkIcon />
+          <span>{repo.forks}</span>
+        </a>
+      )}
+    </div>
+  </Container>
+);
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  border: 1px solid rgb(44, 44, 44);
+  padding: 16px;
+  border-radius: 6px;
+  line-height: 1.5;
+  color: rgba(204, 204, 204, 0.6);
+
+  .title {
+    display: flex;
+    align-items: center;
+    gap: 6px;
+
+    a {
+      font-size: 14px;
+      color: rgb(89, 166, 255);
+      &:hover {
+        text-decoration: underline;
+      }
+    }
+  }
+  .description {
+    flex: 1;
+    margin-top: 8px;
+    font-size: 12px;
+    color: inherit;
+  }
+  .info {
+    display: flex;
+    align-items: center;
+    gap: 16px;
+    margin-top: 8px;
+    font-size: 12px;
+    div,
+    a {
+      display: flex;
+      align-items: center;
+      gap: 6px;
+      color: inherit;
+
+      .language-color {
+        width: 12px;
+        height: 12px;
+        border-radius: 50%;
+      }
+    }
+    a:hover {
+      color: rgb(89, 166, 255);
+    }
+  }
+`;
+
+export default RepoCard;
