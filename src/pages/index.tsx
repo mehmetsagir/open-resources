778ee9f8851053d655cac67b61d065527520a3fa
mehmetsagir <memetsagirint@gmail.com>
2022-07-12 18:31:03 +0300

    add routing to search by choosing random language

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,19 @@
 import type { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 const Home: NextPage = () => {
-  return <div>Home</div>;
+  const router = useRouter();
+
+  const languages = ['JavaScript', 'Rust', 'CSS', 'TypeScript', 'Python'];
+  const selectedLanguage =
+    languages[Math.floor(Math.random() * languages.length)];
+
+  useEffect(() => {
+    router.push(`/results?q=${selectedLanguage}`);
+  }, []);
+
+  return null;
 };
 
 export default Home;
