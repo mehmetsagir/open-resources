1056d2927082c5ac23e92fbbef402b4dee4b9375
mehmetsagir <memetsagirint@gmail.com>
2022-07-13 21:24:15 +0300

    add title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,13 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import Layout from 'src/components/Layout';
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <Layout>
+      <Head>
+        <title>Repo Hunter</title>
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
